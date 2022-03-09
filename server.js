50ea65a9c378dad81ab38707d2b6e1d6e2aebcc8
jajoyce <jjoyce4@gmail.com>
2022-03-09 15:34:55 -0500

    Install pg and Sequelize. Connect Sequlize to db.

--- server.js
+++ server.js
@@ -1,19 +1,41 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const controllers = require('./controllers');
-require('dotenv').config()
+const controllers = require("./controllers");
+require("dotenv").config();
 const { PORT } = process.env;
-const cors = require('cors');
-const morgan = require('morgan');
+const cors = require("cors");
+const morgan = require("morgan");
+
+const { Sequelize } = require("sequelize");
+
+// Option 1: Passing a connection URI
+// const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname') // Example for postgres
+
+// Option 3: Passing parameters separately (other dialects)
+const sequelize = new Sequelize("voter_hub", "jaj", "password", {
+  host: "localhost",
+  dialect: "postgres",
+});
+
+async function pgConnect() {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+}
+
+pgConnect();
 
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 
-app.use('/reps', controllers.civicInfo);
+app.use("/reps", controllers.civicInfo);
 
-app.get('/', (req, res) => {
-    res.send('Hello, world. This is working.')
+app.get("/", (req, res) => {
+  res.send("Hello, world. This is working.");
 });
 
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
