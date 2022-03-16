c79187136c52ea8cbdfad19f4ae9f842ac1bc265
jajoyce <jjoyce4@gmail.com>
2022-03-15 22:25:00 -0400

    Set up and add put and delete routes to noteRepController for authorized users to update and delete their own notes on reps.

--- controllers/noteRepController.js
+++ controllers/noteRepController.js
@@ -12,7 +12,7 @@ router.get("/", authorize, async (req, res) => {
     return res.json(notesRep);
   } catch (err) {
     console.log("GET NOTEREP ERROR", err);
-    return res.status(500).send({
+    return res.status(500).json({
       message: err.message || "An error occured retrieving Notes on Reps.",
     });
   }
@@ -20,7 +20,7 @@ router.get("/", authorize, async (req, res) => {
 
 router.post("/", authorize, async (req, res) => {
   if (!req.body.content || !req.body.repName || !req.body.repOffice) {
-    return res.status(400).send({
+    return res.status(400).json({
       message: "Note Content, Rep's name, and Rep's office are required.",
     });
   } else {
@@ -35,10 +35,49 @@ router.post("/", authorize, async (req, res) => {
       return res.json(noteRep);
     } catch (err) {
       console.log("POST NOTEREP ERROR", err);
-      return res.status(500).send({
+      return res.status(500).json({
         message: err.message || "An error occurred creating new Note on Rep.",
       });
     }
+  }
+});
+
+router.put("/", authorize, async (req, res) => {
+  if (!req.body.id || !req.body.content) {
+    return res.status(400).json("Note ID and content required.");
+  }
+  try {
+    const putContent = { content: req.body.content };
+
+    const updated = await NoteRep.update(putContent, {
+      where: { id: req.body.id, userID: req.userID },
+    });
+
+    if (updated[0]) {
+      console.log("UPDATED NOTE", updated[0]);
+      return res.status(200).json(updated[0]);
+    }
+  } catch (err) {
+    console.log("UPDATE NOTE ERROR", err);
+    res.status(500).json(err.message || "Error occured updating the User.");
+  }
+});
+
+router.delete("/", authorize, async (req, res) => {
+  if (!req.body.id) {
+    return res.status(400).json("Note ID required.");
+  }
+  try {
+    const deleted = await NoteRep.destroy({
+      where: { id: req.body.id, userID: req.userID },
+    });
+    if (deleted) {
+      console.log("DELETED NOTE", deleted);
+      return res.status(200).json(deleted);
+    }
+  } catch (err) {
+    console.log("DELETE NOTE ERROR", err);
+    res.status(500).send(err.message || "Error occurred deleting the User.");
   }
 });
 
