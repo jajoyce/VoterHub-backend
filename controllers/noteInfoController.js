a954c580f21fb0c52e70a6153044474383f4472a
jajoyce <jjoyce4@gmail.com>
2022-03-26 23:02:00 -0400

    Add post route to noteInfoController for authorized user to create new voter info note.

--- controllers/noteInfoController.js
+++ controllers/noteInfoController.js
@@ -18,4 +18,26 @@ router.get("/", authorize, async (req, res) => {
   }
 });
 
+router.post("/", authorize, async (req, res) => {
+  if (!req.body.content) {
+    return res.status(400).json({
+      message: "Note Content is required.",
+    });
+  } else {
+    const newNoteInfo = {
+      content: req.body.content,
+      userID: req.userID,
+    };
+    try {
+      const noteInfo = await NoteInfo.create(newNoteInfo);
+      return res.status(201).json(noteInfo);
+    } catch (err) {
+      console.log("POST NOTEINFO ERROR", err);
+      return res.status(500).json({
+        message: err.message || "An error occurred creating new Note on Info.",
+      });
+    }
+  }
+});
+
 module.exports = router;
